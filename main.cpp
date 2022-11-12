d32d211bdeb1a76628bd78fd398e06ec7fe52ab9
santoni3 <santoni3@illinois.edu>
2022-11-11 22:24:02 -0600

    Simple Read Entire File and Print

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ int main() {
+    ifstream dataset("web-Google.txt");
+    string dataread;
+    if (dataset.is_open()) {
+        dataset >> dataread;
+        cout << dataread << endl;
+    return 0;
+ }
