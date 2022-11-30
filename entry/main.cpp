f2eddbc2df78476c8c497c2b51dccf830fe395ef
Anisha Jog <ajog2@illinois.edu>
2022-11-30 18:28:12 +0000

    skdjgskg

--- entry/main.cpp
+++ entry/main.cpp
@@ -8,7 +8,7 @@ using namespace algos;
     
 int main() {
     cout << "Building graph..." << endl;
-    ifstream dataset("../web.tsv");
+    ifstream dataset("../web-Google.txt");
     Graph g(dataset);
 
     cout << "The graph's size is " << g.getSize() << endl;
@@ -24,8 +24,8 @@ int main() {
     vector<Vertex> bfs_traversal = bfs(g, g.getVertex(0));
 
     cout << "The traversal contains " << bfs_traversal.size() << " vertices." << endl;
-    cout << "The first 10 vertices in the traversal are: " << endl;
-    for (int i = 0; i < 10; i++) {
+    cout << "The first 50 vertices in the traversal are: " << endl;
+    for (int i = 0; i < 50; i++) {
         cout << bfs_traversal.at(i) << " -> ";
     }
     cout << endl;
